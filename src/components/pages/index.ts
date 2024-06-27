f77e7e8feac2639b79a65abeb66d0d42808669ef
JyrwaJR <jyrwaboys@gmail.com>
2024-06-28 00:59:28 +0530

    test husky

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,4 +1,5 @@
 export { AddCentre } from '@components/pages/centre/add-center';
 export { AddDomain } from '@components/pages/domain/add-domain';
 export { CentrePage } from '@components/pages/centre';
+
 export { DomainPage } from '@components/pages/domain';
