d454148f372377bd61af9c8e0800ddfe0bcb7008
JyrwaJR <jyrwaboys@gmail.com>
2024-06-28 01:06:32 +0530

    add center domain

---
+++ src/components/pages/centre-domain/add-center-domain.tsx
@@ -0,0 +1,12 @@
+import EventEmitter from 'events';
+import React from 'react';
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+};
+const AddCentreDomain = ({ onClose, open }: Props) => {
+  return <div>AddCentreDomain</div>;
+};
+
+export default AddCentreDomain;
