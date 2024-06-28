378754232c353bd5d3879e3ee7f6c8bd172f2d23
JyrwaJR <jyrwaboys@gmail.com>
2024-06-28 21:45:31 +0530

    correct typo for centre

--- src/components/pages/centre/index.tsx
+++ src/components/pages/centre/index.tsx
@@ -1,11 +1,8 @@
 'use client';
 import React from 'react';
 
-import { Button } from '@src/components/ui/button';
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/ui/tabs';
 import { ScrollArea } from '@components/ui/scroll-area';
-import { CalendarDateRangePicker } from '@components/date-range-picker';
 import { CenterDomainPage } from './domain';
 import { CenterPageComponents } from './center';
 
@@ -13,19 +10,10 @@ export const CentrePage = () => {
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 p-4 pt-6 space-y-4 md:p-8">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">
-            Hi, Welcome back 👋
-          </h2>
-          <div className="items-center hidden space-x-2 md:flex">
-            <CalendarDateRangePicker />
-            <Button>Download</Button>
-          </div>
-        </div>
         <Tabs defaultValue="overview" className="space-y-4">
           <TabsList>
             <TabsTrigger value="overview">Overview</TabsTrigger>
-            <TabsTrigger value="domain">Add Domain to center</TabsTrigger>
+            <TabsTrigger value="domain">Add Centre Domain</TabsTrigger>
           </TabsList>
           <TabsContent value="overview" className="space-y-4">
             <CenterPageComponents />
