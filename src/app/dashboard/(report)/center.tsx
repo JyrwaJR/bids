f0ed7218f550a4d2f1fdfb316f69c9ebe69ca238
Jyrwa <jyrwaboys@gmail.com>
2024-07-17 01:45:01 +0530

    created report page

---
+++ src/app/dashboard/(report)/center.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReportPage from '@components/pages/reports';
+
+const Page = () => {
+  return <ReportPage />;
+};
+
+Page.propTypes = {};
+
+export default Page;
