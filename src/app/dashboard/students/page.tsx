a569b0ddc87311ad1790ae167c6fb4f497eab1e8
Jyrwa <jyrwaboys@gmail.com>
2024-07-21 11:04:51 +0530

    Student pages
    Created student pages yet to implement list inside it

---
+++ src/app/dashboard/students/page.tsx
@@ -0,0 +1,5 @@
+import StudentsPage from "@components/pages/students";
+
+export default function Page() {
+  return <StudentsPage />;
+}
---
+++ src/components/pages/students/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StudentsPage = () => {
+  return (
+    <div>StudentSPage</div>
+  )
+}
+
+export default StudentsPage
