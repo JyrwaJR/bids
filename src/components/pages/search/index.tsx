51985d939e5dee97929611b5fd58cc1b1aa2d965
jyrwaJR <jyrwaboys@gmail.com>
2024-07-26 17:13:06 +0530

    create search page components

---
+++ src/components/pages/search/index.tsx
@@ -0,0 +1,120 @@
+import { DataTable } from '@components/ui/data-table'
+import { searchStudentByName } from '@src/app/dashboard/registration/_lib/function'
+import { ColumnDef } from '@tanstack/react-table'
+import { useSearchParams } from 'next/navigation'
+import React from 'react'
+import { useMutation, useQuery } from 'react-query'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FieldsIsRequired } from '@constants/index'
+import { z } from 'zod'
+import { FormFieldType } from '@components/index'
+import { studentQueryKey } from '@constants/query-keys'
+import { FormControl, FormField, FormItem, Form } from '@components/ui/form'
+import { Input } from '@components/ui/input'
+import { Button } from '@components/ui/button'
+import { FailedToastTitle } from '@constants/toast-message'
+import { showToast } from '@components/ui/show-toast'
+import { useForm } from 'react-hook-form'
+import Header from '@components/layout/header'
+import { Heading } from '@components/ui/heading'
+const FindStudentModel = z.object({
+  name: z
+    .string({
+      required_error: FieldsIsRequired
+    })
+    .trim()
+});
+const searchStudentFields: FormFieldType[] = [
+  {
+    name: 'name'
+  }
+];
+
+const studentsColumn: ColumnDef<any>[] = [
+  {
+    header: 'Name',
+    accessorKey: 'first_name',
+  },
+  {
+    header: 'Last Name',
+    accessorKey: 'last_name',
+  },
+  {
+    header: 'DOB',
+    accessorKey: 'dob',
+  },
+  {
+    header: 'Email',
+    accessorKey: 'email',
+  }
+]
+export const SearchPage = () => {
+  const search = useSearchParams().get('q')
+  const isSearchApplicants: boolean = search === 'applicants'
+  const url = isSearchApplicants ? 'registration/candidate-registration-list' : ''
+  const form = useForm({
+    defaultValues: {
+      name: ''
+    }
+  })
+  const studentMutation = useMutation({
+    mutationFn: async () => await searchStudentByName(form.watch('name')),
+    mutationKey: [studentQueryKey, form.watch('name')],
+  })
+  const applicantsColumn: ColumnDef<any>[] = [
+    {
+      header: 'Name',
+      accessorKey: 'name',
+      cell: (info) => info.getValue(),
+    }
+  ]
+  const onSubmit = async (data: any) => {
+    try {
+      await studentMutation.mutateAsync()
+    } catch (error: any) {
+      showToast(
+        FailedToastTitle,
+        error.message
+      )
+    }
+  }
+  return (
+    <div className='px-1 space-y-10'>
+      <div className='space-y-2'>
+        <Heading
+          title={`Search ${isSearchApplicants ? 'Applicants' : 'Students'}`}
+          description={``}
+        />
+        <Form {...form}>
+          <form className='flex space-x-2' onSubmit={form.handleSubmit(onSubmit)} >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className='flex w-[30%] space-x-2 items-center'>
+                  <FormControl>
+                    <Input placeholder="Please enter your full name" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button
+              disabled={form.getValues('name') === '' || form.getValues('name') === undefined || studentMutation.isLoading}
+              type="submit">
+              Search
+            </Button>
+          </form>
+        </Form>
+
+      </div>
+      <div className='space-y-2'>
+        <DataTable
+          enableSearch={false}
+          columns={isSearchApplicants ? applicantsColumn : studentsColumn}
+          data={studentMutation.data?.data ? studentMutation.data.data : []}
+          searchKey='name'
+        />
+      </div>
+    </div>
+  )
+}
