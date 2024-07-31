f7161264e4cd90d7d9e26c7553dc2a3aa5931774
jyrwaJR <jyrwaboys@gmail.com>
2024-07-31 19:44:21 +0530

    update heading size

--- src/components/pages/registration/preview-form.tsx
+++ src/components/pages/registration/preview-form.tsx
@@ -1,43 +1,39 @@
-import { Typography } from '@components/index'
-import { Card } from '@components/ui/card'
-import { studentRegistrationFields } from '@constants/input-fields/students'
-import { StudentRegistrationModelWithDomainType } from '@src/app/dashboard/registration/_lib/function'
-import { useRegistrationFields } from '@src/app/dashboard/registration/_lib/useRegistrationFields'
-import { StepsFieldFormT } from '@src/types'
-import React from 'react'
-import { UseFormReturn } from 'react-hook-form'
+import { Typography } from '@components/index';
+import { Card } from '@components/ui/card';
+import { StudentRegistrationModelWithDomainType } from '@src/app/dashboard/registration/_lib/function';
+import { StepsFieldFormT } from '@src/types';
+import React from 'react';
+import { UseFormReturn } from 'react-hook-form';
 
 type Props = {
-  form: UseFormReturn<StudentRegistrationModelWithDomainType>
-  fields: StepsFieldFormT[]
-}
-export const PreviewForm = ({
-  form, fields
-}: Props) => {
-  console.log(form.formState)
+  form: UseFormReturn<StudentRegistrationModelWithDomainType>;
+  fields: StepsFieldFormT[];
+};
+export const PreviewForm = ({ form, fields }: Props) => {
+  console.log(form.formState);
   return (
-    <div className='space-y-5'>
+    <div className="space-y-5">
       {fields.map((field, i) => (
-        <Card key={i} className='flex px-5 py-2 flex-col space-y-4'>
-          <Typography size={'h1'} colors={'primary'} weight={'bold'}>
+        <Card key={i} className="flex flex-col space-y-4 px-5 py-2">
+          <Typography size={'h3'} colors={'primary'} weight={'bold'}>
             {field.name}
           </Typography>
-          <div className='grid py-4 grid-cols-3 gap-4'>
+          <div className="grid grid-cols-3 gap-4 py-4">
             {field.fields.map((item, i) => (
-              <div className='col-span-1 flex gap-4' key={i}>
+              <div className="col-span-1 flex gap-4" key={i}>
+                <div>
+                  <Typography>{item.label}</Typography>
+                </div>
+                <div>
+                  <Typography className="text-muted-foreground">:</Typography>
+                </div>
                 <div>
                   <Typography>
-                    {item.label}
-                  </Typography>
-                </div>
-                <div>
-                  <Typography className='text-muted-foreground'>
-                    :
-                  </Typography>
-                </div>
-                <div>
-                  <Typography >
-                    {form.getValues()[item.name as keyof StudentRegistrationModelWithDomainType] as string}
+                    {
+                      form.getValues()[
+                        item.name as keyof StudentRegistrationModelWithDomainType
+                      ] as string
+                    }
                   </Typography>
                 </div>
               </div>
@@ -46,6 +42,5 @@ export const PreviewForm = ({
         </Card>
       ))}
     </div>
-  )
-}
-
+  );
+};
