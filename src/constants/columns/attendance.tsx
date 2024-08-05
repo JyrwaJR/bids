0ae602d68bc5cf7f82ea7d78893c181938525207
jyrwaJR <jyrwaboys@gmail.com>
2024-08-06 00:04:46 +0530

    change column

--- src/constants/columns/attendance.tsx
+++ src/constants/columns/attendance.tsx
@@ -3,11 +3,15 @@ import { ColumnDef } from '@tanstack/react-table';
 
 export const attendanceColumn: ColumnDef<AttendanceModelType>[] = [
   {
-    accessorKey: 'batch_id',
+    accessorKey: 'batch',
     header: 'Batch'
   },
   {
     accessorKey: 'date',
     header: 'Date'
+  },
+  {
+    accessorKey: 'status',
+    header: 'Status'
   }
 ];
