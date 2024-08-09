e55699ac00aa25cfbfc89fa0ec7b6e0bc5557b50
jyrwaJR <jyrwaboys@gmail.com>
2024-08-09 17:54:39 +0530

    change validiation

--- src/constants/regex/image.ts
+++ src/constants/regex/image.ts
@@ -4,12 +4,11 @@ export const imageValidation = z
   .instanceof(File, {
     message: 'Please select an image'
   })
-  .refine((file) => file !== undefined && file !== null, {
-    message: 'Please select an image'
+  // Corrected: Checks the file size, ensuring it is less than or equal to MAX_UPLOAD_SIZE.
+  .refine((file) => file && file.size <= MAX_UPLOAD_SIZE, {
+    message: 'File size must be less than 2MB'
   })
-  .refine((file) => {
-    return file && file.size <= MAX_UPLOAD_SIZE;
-  }, 'File size must be less than 2MB')
-  .refine((file) => {
-    return file && ACCEPTED_FILE_TYPES.includes(file.type);
-  }, 'File must be a PNG');
+  // Corrected: Ensures the file type is one of the accepted types.
+  .refine((file) => file && ACCEPTED_FILE_TYPES.includes(file.type), {
+    message: 'File must be a PNG/jpg/jpeg'
+  });
