91eac9e672ae0514e0c3c5d2f5f46c052e7f3512
jyrwaJR <jyrwaboys@gmail.com>
2024-08-14 18:14:42 +0530

    revmove popup

--- src/components/pages/centre/domain/index.tsx
+++ src/components/pages/centre/domain/index.tsx
@@ -7,46 +7,102 @@ import { showToast } from '@components/ui/show-toast';
 import { domainColumn } from '@constants/columns';
 import { useCQuery } from '@hooks/useCQuery';
 import { ColumnDef } from '@tanstack/react-table';
-import { Plus } from 'lucide-react';
-import React, { useState } from 'react';
-import { AddCentreDomain } from '@components/pages/centre/domain/add-center-domain';
-import { DomainModelType } from '@src/models';
-import { domainQueryKey } from '@constants/query-keys';
+import React, { useEffect, useState } from 'react';
+import {
+  CenterDomainModel,
+  CenterDomainModelType,
+  DomainModelType
+} from '@src/models';
+import { centreQueryKey, domainQueryKey } from '@constants/query-keys';
+import { useCMutation } from '@hooks/useCMutation';
+import { FailedToastTitle, SuccessToastTitle } from '@constants/toast-message';
+import { FormFieldType, OptionsT } from '@components/form/type';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form } from '@components/ui/form';
+import { CForm } from '@components/form';
 interface ColumnType extends DomainModelType {
   id: string;
 }
 const CenterDomainPage = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { data, isLoading } = useCQuery({
+  const [isSelectedDomId, setIsSelectedDomId] = useState<string[]>([]);
+  const form = useForm<CenterDomainModelType>({
+    resolver: zodResolver(CenterDomainModel),
+    defaultValues: {
+      domain_id: isSelectedDomId
+    }
+  });
+  const { data: domainData, isLoading: domainLoading } = useCQuery({
     url: 'domain',
     queryKey: domainQueryKey
   });
-  const [isSelectedDomId, setIsSelectedDomId] = useState<string[]>([]);
 
+  const { mutateAsync, isLoading } = useCMutation({
+    url: 'centre-domain/save',
+    method: 'POST',
+    queryKey: [...domainQueryKey, ...centreQueryKey]
+  });
+
+  const {
+    data: centreData,
+    isLoading: loading,
+    isFetched
+  } = useCQuery({
+    url: 'centre',
+    queryKey: centreQueryKey
+  });
+  const onSubmit: SubmitHandler<CenterDomainModelType> = async (data) => {
+    try {
+      if (isSelectedDomId.length > 0) {
+        const res = await mutateAsync({
+          ...data,
+          domain_id: isSelectedDomId
+        });
+        if (res.success) {
+          setIsSelectedDomId([]);
+          showToast(SuccessToastTitle, res.message);
+          return;
+        }
+      }
+      showToast(FailedToastTitle, 'Please select atleast one domain');
+      return;
+    } catch (error: any) {
+      showToast(FailedToastTitle, error.message);
+      return;
+    }
+  };
+
+  const centreOptions: OptionsT[] | undefined =
+    isFetched &&
+    !isLoading &&
+    centreData.data.map((item: any) => ({
+      label: item.name,
+      value: item.id
+    }));
+
+  const fields: FormFieldType[] = [
+    {
+      name: 'centre_id',
+      label: 'Select Centre',
+      select: true,
+      options: centreOptions,
+      required: true
+    }
+  ];
   const columns: ColumnDef<ColumnType | any>[] = [
     {
       id: 'select',
-      header: ({ table }) => (
-        <Checkbox
-          checked={table.getIsAllPageRowsSelected()}
-          onCheckedChange={(value) => {
-            table.toggleAllPageRowsSelected(!!value);
-            const row = table.getRowModel().rows;
-            if (value) {
-              setIsSelectedDomId(row.map((row) => row.original.id));
-            } else {
-              setIsSelectedDomId([]);
-            }
-          }}
-          aria-label="Select all"
-        />
-      ),
       cell: ({ row }) => (
         <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => {
-            row.getToggleSelectedHandler()(!!value);
-            setIsSelectedDomId([...isSelectedDomId, row.original.id]);
+          checked={isSelectedDomId.includes(row.original.id)}
+          onCheckedChange={() => {
+            setIsSelectedDomId((prevSelected) => {
+              if (prevSelected.includes(row.original.id)) {
+                return prevSelected.filter((id) => id !== row.original.id);
+              } else {
+                return [...prevSelected, row.original.id];
+              }
+            });
           }}
           aria-label="Select row"
         />
@@ -56,40 +112,43 @@ const CenterDomainPage = () => {
     },
     ...domainColumn
   ];
-  const onCLickAddDomain = () => {
-    if (isSelectedDomId.length > 0) {
-      setIsOpen(true);
-      return;
-    }
-    showToast('Failed', 'Please select domain to add to a center');
-    return;
-  };
   return (
     <>
       <div className="flex-1 space-y-4">
-        <div className="flex items-start justify-between">
+        <div className="flex items-start justify-start">
           <Heading
             title={`Center Domain`}
-            description="Please select a domain to add a center"
+            description="Please select a domain center"
           />
-          <Button className="text-xs md:text-sm" onClick={onCLickAddDomain}>
-            <Plus className="mr-2 h-4 w-4" /> Add New
-          </Button>
         </div>
+        <Separator />
+        <Form {...form}>
+          <div className="flex items-center space-x-2">
+            <CForm
+              form={form}
+              loading={isLoading || loading || !isFetched}
+              fields={fields}
+              className="w-full"
+            />
+            <div className="pt-4">
+              <Button
+                onClick={form.handleSubmit(onSubmit)}
+                disabled={
+                  isLoading || !isFetched || isSelectedDomId.length === 0
+                }
+              >
+                Save{' '}
+              </Button>
+            </div>
+          </div>
+        </Form>
         <Separator />
         <DataTable
           searchKey="name"
           columns={columns}
-          data={isLoading ? [] : data.data}
+          data={domainLoading ? [] : domainData.data}
         />
       </div>
-      {isOpen && (
-        <AddCentreDomain
-          domain_id={isSelectedDomId}
-          open={isOpen}
-          onClose={() => setIsOpen(false)}
-        />
-      )}
     </>
   );
 };
