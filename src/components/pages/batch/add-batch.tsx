738eeb580077f8ae529e74e280702c9d9744ec1e
jyrwaJR <jyrwaboys@gmail.com>
2024-08-25 21:59:20 +0530

    create page batch

--- src/components/pages/batch/add-batch.tsx
+++ src/components/pages/batch/add-batch.tsx
@@ -1,65 +1,126 @@
-// import { Form, FormFieldType } from '@components/form';
-// import { OptionsT } from '@components/form/type';
-// import { Button } from '@components/ui/button';
-// import { Checkbox } from '@components/ui/checkbox';
-// import { DataTable } from '@components/ui/data-table';
-//
-// import {
-//   Dialog,
-//   DialogContent,
-//   DialogDescription,
-//   DialogHeader,
-//   DialogTitle
-// } from '@components/ui/dialog';
-// import { showToast } from '@components/ui/show-toast';
-// import { domainColumn } from '@constants/columns';
-// import { batchFields } from '@constants/input-fields';
-// import {
-//   batchQueryKey,
-//   domainQueryKey,
-//   staffQueryKey
-// } from '@constants/query-keys';
-// import { FailedToastTitle } from '@constants/toast-message';
-// import { zodResolver } from '@hookform/resolvers/zod';
-// import { useCMutation } from '@hooks/useCMutation';
-// import { useCQuery } from '@hooks/useCQuery';
-// import { BatchModel, BatchModelType, DomainModelType } from '@src/models';
-// import { ColumnDef } from '@tanstack/react-table';
-// import React, { useEffect, useState } from 'react';
-// import { SubmitHandler, useForm } from 'react-hook-form';
-//
-// type Props = {
-//   open: boolean;
-//   onClose: () => void;
-//   projectId: string;
-// };
-// interface ColType extends DomainModelType {
-//   id: string;
-// }
-// // export const AddBatch = ({ open, onClose, projectId }: Props) => {
-// //   return (
-// //     <Dialog open={open} onOpenChange={onClose}>
-// //       <DialogContent>
-// //         <DialogHeader>
-// //           <DialogTitle>
-// //             {isSelectedDomain ? 'Add Batch' : 'Select Domain'}
-// //           </DialogTitle>
-// //           <DialogDescription>
-// //             {isSelectedDomain ? 'Add Batch' : 'Select Domain to add batch'}
-// //           </DialogDescription>
-// //         </DialogHeader>
-// //         {isSelectedDomainId.length > 0 && isSelectedDomain ? (
-// //         ) : (
-// //           <>
-// //             <Button
-// //               disabled={loading || isLoading || isSelectedDomainId.length === 0}
-// //               onClick={() => setSelectedDomain(true)}
-// //             >
-// //               Done
-// //             </Button>
-// //           </>
-// //         )}
-// //       </DialogContent>
-// //     </Dialog>
-// //   );
-// // };
+import { Form, FormFieldType } from '@src/components';
+import { domainFormFields as domainFields } from '@constants/input-fields/domain/domain-form-fields';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  BatchModel,
+  BatchModelType,
+  DomainModel,
+  DomainModelType
+} from '@src/models';
+import React from 'react';
+import { SubmitHandler, useForm, useWatch } from 'react-hook-form';
+import { useCMutation } from '@hooks/useCMutation';
+import { showToast } from '@components/ui/show-toast';
+import { FailedToastTitle, SuccessToastTitle } from '@constants/toast-message';
+import { AxiosError } from 'axios';
+import { appendNonFileDataToFormData } from '@lib/appendNoneFileDataToFileData';
+import {
+  batchQueryKey,
+  domainQueryKey,
+  projectsQueryKey,
+  staffQueryKey
+} from '@constants/query-keys';
+import { Separator } from '@components/ui/separator';
+import { Heading } from '@components/ui/heading';
+import { useCategorySelectOptions } from '@hooks/useCategorySelectOptions';
+import { batchFields } from '@constants/input-fields';
+import { useCQuery } from '@hooks/useCQuery';
+import { OptionsT } from '@components/form/type';
+
+export const AddBatch = () => {
+  const form = useForm<BatchModelType>({
+    resolver: zodResolver(BatchModel)
+  });
+  const { options } = useCategorySelectOptions();
+  const { mutateAsync, isLoading } = useCMutation({
+    url: 'batch/save',
+    method: 'POST',
+    queryKey: batchQueryKey
+  });
+  const watchProjectId = useWatch({
+    control: form.control,
+    name: 'project_id'
+  });
+  const staffQuery = useCQuery({
+    url: `staff`,
+    queryKey: staffQueryKey
+  });
+  const domainQuery = useCQuery({
+    url: `project-domain/get-domain-by-project/${form.watch('project_id')}`,
+    queryKey: [projectsQueryKey, domainQueryKey],
+    enabled: !!watchProjectId
+  });
+
+  const isDomainOptions: OptionsT[] = domainQuery.data?.data?.map(
+    (item: any) => ({
+      label: item.domain,
+      value: item.domain_id
+    })
+  );
+  const staffOptions: OptionsT[] = staffQuery.data?.data?.data?.map(
+    (item: any) => ({
+      label: item.name,
+      value: item.id
+    })
+  );
+
+  const onSubmit: SubmitHandler<DomainModelType> = async (data) => {
+    try {
+      if (!data.guide && !data.curriculum) {
+        const res = await mutateAsync(data);
+        if (res.success) {
+          showToast(SuccessToastTitle, res.message);
+          form.reset();
+        }
+      }
+    } catch (error: any) {
+      if (error instanceof AxiosError) {
+        showToast(FailedToastTitle, error.response?.data.message);
+      } else {
+        showToast(FailedToastTitle, error.message);
+      }
+    }
+  };
+  const updatedFields: FormFieldType[] = [
+    {
+      name: 'project_id',
+      label: 'Select Project',
+      select: true,
+      options: options.projects
+    },
+    {
+      name: 'domain_id',
+      label: 'Domain',
+      select: true,
+      readOnly: !!watchProjectId ? false : true,
+      options: isDomainOptions
+    },
+    ...batchFields,
+    {
+      name: 'trainer_id',
+      label: 'Trainer',
+      select: true,
+      options: staffOptions,
+      required: true
+    },
+    {
+      name: 'support_trainer_id',
+      options: staffOptions,
+      label: 'Support Trainer',
+      select: true
+    }
+  ];
+  return (
+    <div className="flex-1 space-y-4">
+      <Heading title={`Add Batch`} description="Please enter the  following" />
+      <Separator />
+      <Form
+        fields={updatedFields}
+        form={form}
+        loading={isLoading}
+        onSubmit={onSubmit}
+        className="sm:col-span-6 md:col-span-4"
+      />
+    </div>
+  );
+};
--- src/components/pages/batch/index.tsx
+++ src/components/pages/batch/index.tsx
@@ -3,7 +3,11 @@ import { Heading } from '@components/ui/heading';
 import { BatchColumn } from '@constants/columns/batch-column';
 import { useCQuery } from '@hooks/useCQuery';
 import { batchQueryKey } from '@constants/query-keys';
+import { Button } from '@components/ui/button';
+import { PlusIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 const BatchPage = () => {
+  const router = useRouter();
   const batchQuery = useCQuery({
     url: 'batch',
     queryKey: batchQueryKey
@@ -12,8 +16,11 @@ const BatchPage = () => {
   return (
     <>
       <div className="flex-1 space-y-4">
-        <div className="">
+        <div className="flex items-start justify-between">
           <Heading title={`Batch`} description="Manage Batch table" />
+          <Button onClick={() => router.push('/dashboard/batch/add-batch')}>
+            <PlusIcon /> Add Batch
+          </Button>
         </div>
         <DataTable
           searchKey="batch_code"
